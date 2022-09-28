611a55bb8019c349551385c0caaf382e2769cce9
Your Name01~ <fateme.ebrahimzadeh20@gmail.com>
2022-09-28 15:15:45 +0330

    changing nodes support ,9/28/2022

--- final_main.py
+++ final_main.py
@@ -179,51 +179,89 @@ server_state = True
 
 
 async def main(clientMqtt, server_state):
-
+    structPad = set_structure('>8sh')
+    structData = set_structure('<hffbb')
+    firstTime = True
+    loop = asyncio.get_event_loop()
     try:
         while clientMqtt !=0:
-            # dataStart = True
-            # while dataStart:
-            #     dataDict1, opcServers = await create_database()
-            #     if opcServers != []:
-            #         dataStart = False
-                    # opcServers = dataDict[1]["opcServer"]
-                    # signals = []
-                    # for i in dataDict:
-                    #     print(dataDict[i]["opcServer"])
-                    #     if opcServers == dataDict[i]["opcServer"]:
-                #     #         signals.append(dataDict[i]["signals"][0])
-                # print(dataDict1)
-                # print(opcServers)
-            # clientMqtt.unsubscribe('send_opc_tag')
-            # while server_state:
-            #     dataDict, client, server_state = await create_dataDict(dataDict1, opcServers, server_state)
-            # print("DONE")
-            structPad = set_structure('>8sh')
-            structData = set_structure('<hffbb')
-            firstTime = True
-            await data_catchSend(structPad, structData, clientMqtt, server_state, firstTime)
+            z = loop.create_task(create_database(firstTime))
+            if firstTime:
+                dataBase, opcServers, firstTime = await z
+                dataDict, client, server_state = await create_dataDict(dataBase, opcServers, server_state)
+
+                # while server_state:
+
+
+            else:
+                z = asyncio.create_task(create_database(firstTime))
+                dataBase, opcServers, firstTime = await z
+                # while server_state:
+
+                if dataBase != 0:
+                    server_state = True
+                    dataDict, client, server_state = await asyncio.create_task(
+                        create_dataDict(dataBase, opcServers, server_state))
+
+                else:
+                    # dataDict = firstData
+
+                    pass
+            # firstData = dataDict
+            x = loop.create_task(get_values(dataDict, client))
+            y = loop.create_task(get_timesync(q))
+            timeSync = await y
+            dataDict = await x
+            print(timeSync)
+            value = []
+            percentt = []
+            id = []
+            for i in dataDict:
+                id.append(i)
+                if dataDict[i]["nodeList"] != []:
+                    if dataDict[i]["values"][0] is None:
+                        value.append(00.00)
+                        dataDict[i]["percent"] = 00.00
+                        percentt.append(0.0)
+                    else:
+                        value.append(dataDict[i]["values"][0])
+                        dataDict[i]["percent"] = percentage(dataDict[i]["VMX"], dataDict[i]["VMN"],
+                                                            dataDict[i]["values"])
+                        percentt.append(dataDict[i]["percent"][0])
+                    dataDict[i]["timeStamp"] = timeSync
+                    dataDict[i]['bufferSize'] = await buffer_data_get_padding(structPad, dataDict[i]["bufferSize"], 0,
+                                                                              timeSync, 1)
+                    # dataDict[i]['bufferSize'] = await buffer_data_get(structData, dataDict[i]["bufferSize"], i, dataDict[i])
+            values = {
+                "id": id,
+                "values": value,
+                "percent": percentt,
+                "buffer": estimate_buffer_size(len(value))
+            }
+            buffer = values["buffer"]
+            await buffer_data_get_padding(structPad, buffer, 0, timeSync, len(value))
+            await buffer_data_get(structData, buffer, values)
+            i = 0
+            print(values["id"])
+            # await asyncio.sleep(0.8)
+            clientMqtt.publish('omid_test_topic', buffer)
+
+            # await data_catchSend(structPad, structData, clientMqtt, server_state, firstTime)
 
     except:
-
-        return 0
-
+        await main(clientMqtt, server_state)
 
 if __name__ == "__main__":
     try:
-        loop = asyncio.get_event_loop()
 
         set_connection = True
         mqttTopic = [("send_opc_tag", 0), ("TimeSync", 0)]
         clientMqtt = brokerConnection(set_connection, mqttTopic)
         stayOnDemand(clientMqtt)
-        task = loop.create_task(main(clientMqtt, server_state))
+        asyncio.run(main(clientMqtt, server_state))
         # asyncio.run(main(clientMqtt, server_state))
         # asyncio.run(checkMessageFrom(q))
-        loop.run_forever()
-        tasks = asyncio.all_tasks(loop=loop)
-        group = asyncio.gather(*tasks, return_exceptions=True)
-        loop.run_until_complete(group)
+
         # loop.run_forever()
     except KeyboardInterrupt:
         pass
@@ -233,4 +271,9 @@ if __name__ == "__main__":
             time.sleep(1)
             print(f"Reconnect to Server in {3 - i} ...")
         # mqtt_disconnect(clientMqtt)
+        set_connection = True
+        server_state = False
+        mqttTopic = [("send_opc_tag", 0), ("TimeSync", 0)]
+        clientMqtt = brokerConnection(set_connection, mqttTopic)
+        stayOnDemand(clientMqtt)
         asyncio.run(main(clientMqtt, server_state))
